8d05ace9af2c28a55c0499053cab623b6a36755b
Maksym <rendon4ik@gmail.com>
2024-12-28 19:44:20 +0200

    Change server port

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
   ],
+  server: {
+    port: 5174,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
